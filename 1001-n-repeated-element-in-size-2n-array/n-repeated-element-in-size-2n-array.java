ed3547e3f50d27bb97f98253f59da259c09352f4
Kavya Sree B S <kavyasree614@gmail.com>
2026-01-02 21:50:01 +0530

    Time: 629 ms (5.06%) | Memory: 47.9 MB (6.11%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -4,10 +4,9 @@ class Solution {
         for(int i=0;i<nums.length-1;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]==nums[j]){
-                    n = nums[i];
+                    n=nums[i];
                 }
             }
-
         }
         return n;
     }
