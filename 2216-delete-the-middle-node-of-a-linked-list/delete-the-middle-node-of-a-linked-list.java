dc8b164bdd70fbeea5c8811001bc9025d1a65a98
Kavya Sree B S <kavyasree614@gmail.com>
2026-01-29 20:29:16 +0530

    Time: 3 ms (99.46%) | Memory: 202.7 MB (52.86%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
@@ -21,7 +21,7 @@ class Solution {
             slow = slow.next;
             fast = fast.next.next;
         }
-            prev.next = slow.next;                                        
+     prev.next = prev.next.next;                                        
         return head;
     }
 }
