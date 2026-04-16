becd0412006fc8842aa91a4850f1b4c9e534f1b0
Kavya Sree B S <kavyasree614@gmail.com>
2026-04-16 17:45:54 +0530

    Time: 1 ms (100.00%) | Memory: 48 MB (64.89%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+
+        for(int num : nums){
+            xor ^= num;
+        }
+
+        int mask = xor & (-xor);
+
+        int a = 0, b = 0;
+
+        for(int num : nums){
+            if((num & mask) == 0){
+                a ^= num;
+            } else {
+                b ^= num;
+            }
+        }
+
+        return new int[]{a,b};
+    }
+}
