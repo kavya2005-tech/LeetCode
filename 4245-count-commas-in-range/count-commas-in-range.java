09e342eee953ae849b7f4ab7d15edc82cf8d7883
Kavya Sree B S <kavyasree614@gmail.com>
2026-06-16 12:10:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (76.60%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,10 +1,5 @@
 class Solution {
     public int countCommas(int n) {
-        int count = 0;
-        if(n<999){
-            return 0;
-        }
-        count = n-999;
-        return count;
+        return n > 999 ? n - 999 : 0;
     }
 }
