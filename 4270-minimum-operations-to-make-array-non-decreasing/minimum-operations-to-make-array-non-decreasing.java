f378d0ff4da304e097f81deaa4a4ec1075a43e9c
Kavya Sree B S <kavyasree614@gmail.com>
2026-06-17 12:37:40 +0530

    Time: 3 ms (98.67%) | Memory: 87.3 MB (91.36%) - LeetSync

---
+++ 4270-minimum-operations-to-make-array-non-decreasing/minimum-operations-to-make-array-non-decreasing.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long minOperations(int[] nums) {
+        int n = nums.length;
+        long ans = 0;
+        for(int i=1;i<n;i++){
+            if(nums[i]<nums[i-1]){
+                ans += (nums[i-1] - nums[i]);
+                
+            }
+        }
+        return ans;
+    }
+}
