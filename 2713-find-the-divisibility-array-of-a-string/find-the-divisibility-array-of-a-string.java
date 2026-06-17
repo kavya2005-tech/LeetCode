e05fbef095d6b501576b12a781b0765246cede27
Kavya Sree B S <kavyasree614@gmail.com>
2026-06-17 10:22:18 +0530

    Time: 7 ms (69.61%) | Memory: 81.3 MB (63.73%) - LeetSync

--- 2713-find-the-divisibility-array-of-a-string/find-the-divisibility-array-of-a-string.java
+++ 2713-find-the-divisibility-array-of-a-string/find-the-divisibility-array-of-a-string.java
@@ -4,7 +4,7 @@ class Solution {
         int[] arr = new int[n];
         long rev =0;
         for(int i=0;i<n;i++){
-            int dig = word.charAt(i) - '0';
+            int dig = word.charAt(i)-'0' ;
             rev = (rev * 10 + dig) % (long)m;
             if (rev == 0) {
                 arr[i] = 1;
