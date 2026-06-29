4f40fc5668d68eb09ddae265202c1ad747bf0521
Kavya Sree B S <kavyasree614@gmail.com>
2026-06-29 12:05:27 +0530

    Time: 2 ms (100.00%) | Memory: 199.1 MB (43.68%) - LeetSync

---
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean doesValidArrayExist(int[] derived) {
+        int n = derived.length;
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum+=derived[i];
+        }
+        return sum%2==0;
+    }
+}
