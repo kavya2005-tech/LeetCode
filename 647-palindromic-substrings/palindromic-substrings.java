8156e73a6cfee9e882308379f9cd7a7c53ddfc34
Kavya Sree B S <kavyasree614@gmail.com>
2026-07-02 10:38:52 +0530

    Time: 6 ms (60.49%) | Memory: 42.5 MB (95.91%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int count = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            int left = i, right = i;
+            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+                count++;
+                left--;
+                right++;
+            }
+            left = i; 
+            right = i + 1;
+            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+                count++;
+                left--;
+                right++;
+            }
+        }
+        return count;
+    }
+}
